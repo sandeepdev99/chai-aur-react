676abf73587d029657651a37387be3ef6ff7c54c
sandeepdev99 <sandeep99.dev@gmail.com>
2025-11-11 11:35:45 +0530

    my own react element - anotherElement

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
 import Chai from "./chai"
 
 function App() {
+  
+  const username = 'sandy';
 
   return (
+    <>
     <Chai />
+    <h1>react heading with value {username? username + '123': 'null'}</h1></>
 
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const username = 'sandy';
+
+const anotherElement = React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'click me ',
+  username // evaluated epression
+);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  anotherElement
 )
