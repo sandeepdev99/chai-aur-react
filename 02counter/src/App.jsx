7569b3eb39efff0f0432bc5365d32760bb09beb9
sandeepdev99 <sandeep99.dev@gmail.com>
2025-11-11 12:31:37 +0530

    created 03counter project, using useState hook.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const [counter, setCounter] = useState(15);
+  //let counter = 15;
+  const addValue = () =>{
+    if(counter < 30){
+    console.log(counter, "value added", Math.random());
+    setCounter(counter + 3); ////counter += 1;
+    console.log(counter + 3 , "value added", Math.random());
+    }
+    
+  }
+
+  const subValue = () =>{
+
+    if(counter <= 0) 
+      return
+    console.log(counter, "value added", Math.random());
+    setCounter(counter - 2); //counter -= 1;
+    console.log(counter - 2, "value added", Math.random());
+    
+  }
+  return (
+    <>
+    <h1>Chai aur React {counter}</h1>
+    <h2>counter value: {counter}</h2>
+    <button onClick={addValue}>Add value</button>
+    <br />
+    <button onClick={subValue}>subtract value</button>
+
+    <footer>this is footer {counter}</footer>
+    </> 
+  )
+}
+
+export default App
